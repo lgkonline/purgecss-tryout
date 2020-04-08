61279caa0fd0476d7a43c48ff6b1d378fc1f36ad
Lars Gerrit Kliesing <lgk@patorg.org>
2020-04-08 15:15:13 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# PurgeCSS Tryout
+
+Dies ist ein Beispiel-Projekt, um PurgeCSS auszuprobieren.
+
+Dieses React-Projekt wurde von Hand erstellt und nicht mit create-react-app.  
+Anschließend habe ich diese Anleitung befolgt, um PurgeCSS zu installieren: https://purgecss.com/plugins/webpack.html
+
+---
+
+This is a example projekt to try out PurgeCSS.
+
+This React project is created by hand and not with create-react-app.  
+Then I followed this construction to install PurgeCSS: https://purgecss.com/plugins/webpack.html
+
+## (Take a look at docs/main.css and see how tiny it is by having Bootstrap installed!)
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const globAll = require("glob-all");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+
+const PATHS = {
+    src: path.join(__dirname, "src"),
+    dist: path.join(__dirname, "docs")
+};
+
+module.exports = {
+    context: PATHS.src,
+    entry: [
+        "./main.js",
+    ],
+    output: {
+        path: PATHS.dist,
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    "babel-loader",
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, "node_modules"),
+        ],
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new PurgecssPlugin({
+            paths: globAll.sync([`${PATHS.src}/**/*`, `${PATHS.dist}/**/*`], { nodir: true })
+        })
+    ]
+};
